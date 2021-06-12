c490c70d8680e996aa9af8adfe931ae4be8fc5cc
nin-jin <nin-jin@users.noreply.github.com>
2021-06-12 08:30:18 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -8690,6 +8690,7 @@ var $;
         }
         Store() {
             const obj = new this.$.$mol_store_shared();
+            obj.server = () => "wss://45.9.27.15:3000";
             return obj;
         }
         body() {
--- node.test.js
+++ node.test.js
@@ -8682,6 +8682,7 @@ var $;
         }
         Store() {
             const obj = new this.$.$mol_store_shared();
+            obj.server = () => "wss://45.9.27.15:3000";
             return obj;
         }
         body() {
