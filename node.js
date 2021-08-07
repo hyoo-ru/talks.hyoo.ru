f9b4a230e1b73b151d3af9eef71863b9cb9ee9e2
nin-jin <nin-jin@users.noreply.github.com>
2021-08-07 07:14:12 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -9348,7 +9348,6 @@ var $;
     var $$;
     (function ($$) {
         class $mol_embed_native extends $.$mol_embed_native {
-            dom_node;
             loaded() {
                 const node = this.dom_node();
                 this.uri_resource();
