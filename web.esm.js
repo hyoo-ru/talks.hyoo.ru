fc64b431d2cb8a8c48487b76ba20b7fbd4e48975
nin-jin <nin-jin@users.noreply.github.com>
2021-08-14 07:14:00 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -9905,7 +9905,7 @@ var $;
     if (typeof window === 'undefined') {
         self.addEventListener('message', async (event) => {
             let { context: title, message: body, uri: data } = event.data;
-            const tag = title;
+            const tag = data;
             const existen = await $.$mol_service().getNotifications({ tag });
             for (const not of existen) {
                 if (not.body.indexOf(body) !== -1)
