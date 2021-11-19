4e8ff508714ec84058c7a62604f72289b8fc6aa5
nin-jin <nin-jin@users.noreply.github.com>
2021-11-19 07:16:23 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -7402,6 +7402,9 @@ var $;
         static href(next) {
             return next || process.argv.slice(2).join(' ');
         }
+        static href_normal() {
+            return this.link({});
+        }
         static dict(next) {
             if (next !== void 0)
                 this.href(this.make_link(next));
@@ -7464,6 +7467,9 @@ var $;
     __decorate([
         $.$mol_mem
     ], $mol_state_arg, "href", null);
+    __decorate([
+        $.$mol_mem
+    ], $mol_state_arg, "href_normal", null);
     __decorate([
         $.$mol_mem
     ], $mol_state_arg, "dict", null);
@@ -7541,7 +7547,7 @@ var $;
                 return new URL(this.uri(), base);
             }
             current() {
-                const base = this.$.$mol_state_arg.href();
+                const base = this.$.$mol_state_arg.href_normal();
                 const target = this.uri_native().toString();
                 if (base === target)
                     return true;
