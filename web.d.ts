8f7a5bc5b625dad552be83719b6dfd72b7be12b6
nin-jin <nin-jin@users.noreply.github.com>
2022-02-02 07:18:23 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -2615,7 +2615,7 @@ declare namespace $ {
 declare namespace $.$$ {
     class $mol_embed_native extends $.$mol_embed_native {
         window(): Window;
-        load(frame: HTMLIFrameElement, uri: string): Promise<Window>;
+        load(frame: HTMLIFrameElement): Promise<Window>;
         uri_resource(): string;
         uri_listener(): $mol_dom_listener;
         uri_change(event?: MessageEvent<[string, string]>): void;
