74a296e72cd1ebc78dd0722beb5eb7f498c92b84
nin-jin <nin-jin@users.noreply.github.com>
2022-11-27 07:18:44 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -8909,6 +8909,9 @@ var $;
             }
             return chunks.join(' ');
         }
+        static go(next) {
+            this.href(this.make_link(next));
+        }
         constructor(prefix = '') {
             super();
             this.prefix = prefix;
@@ -8940,6 +8943,9 @@ var $;
     __decorate([
         $mol_mem_key
     ], $mol_state_arg, "value", null);
+    __decorate([
+        $mol_action
+    ], $mol_state_arg, "go", null);
     $.$mol_state_arg = $mol_state_arg;
 })($ || ($ = {}));
 //mol/state/arg/arg.node.ts
