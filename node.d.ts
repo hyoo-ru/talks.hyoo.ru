cd5f688bd427cf4165882444d2da0d465b534e4a
nin-jin <nin-jin@users.noreply.github.com>
2023-03-27 07:18:30 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2936,10 +2936,8 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_button_copy extends $mol_button_minor {
-        data(): {
-            "text/plain": Blob;
-            "text/html": Blob;
-        };
+        blobs(): readonly Blob[];
+        data(): {};
         sub(): readonly any[];
         text(): string;
         text_blob(next?: any): Blob;
@@ -2956,6 +2954,9 @@ declare namespace $ {
 
 declare namespace $.$$ {
     class $mol_button_copy extends $.$mol_button_copy {
+        data(): {
+            [k: string]: Blob;
+        };
         html(): string;
         attachments(): ClipboardItem[];
         click(event?: Event): void;
--- web.d.ts
+++ web.d.ts
@@ -2906,10 +2906,8 @@ declare namespace $ {
 
 declare namespace $ {
     class $mol_button_copy extends $mol_button_minor {
-        data(): {
-            "text/plain": Blob;
-            "text/html": Blob;
-        };
+        blobs(): readonly Blob[];
+        data(): {};
         sub(): readonly any[];
         text(): string;
         text_blob(next?: any): Blob;
@@ -2926,6 +2924,9 @@ declare namespace $ {
 
 declare namespace $.$$ {
     class $mol_button_copy extends $.$mol_button_copy {
+        data(): {
+            [k: string]: Blob;
+        };
         html(): string;
         attachments(): ClipboardItem[];
         click(event?: Event): void;
