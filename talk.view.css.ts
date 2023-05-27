67f1111f056012bad4704f1eddbfd2ae5d82a742
jin <jin@hyoo.ru>
2023-05-27 22:33:57 +0300

    build fix

--- talk.view.css.ts
+++ talk.view.css.ts
@@ -1,8 +1,5 @@
 namespace $.$$ {
 	
-	const { rem } = $mol_style_unit
-	const { url } = $mol_style_func
-	
 	$mol_style_define( $hyoo_talks, {
 		
 		'@': {
@@ -23,7 +20,7 @@ namespace $.$$ {
 		Roster: {
 			
 			flex: {
-				basis: rem(20),
+				basis: `20rem`,
 				shrink: 0,
 			},
 			
@@ -43,27 +40,6 @@ namespace $.$$ {
 				grow: 0,
 				shrink: 0,
 			},
-		},
-		
-		Placeholder: {
-			
-			Head: {
-				padding: 0,
-			},
-			
-			Body: {
-				padding: 0,
-			},
-			
-			
-			'@': {
-				hyoo_talks_placeholder_transparent: {
-					'true': {
-						backgroundColor: 'transparent',
-					}
-				}
-			},
-		
 		},
 		
 		Chat_unread_count: {
