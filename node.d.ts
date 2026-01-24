7fecc7f6fd52869bc1cf80dcd8d63942dad7a826
nin-jin <nin-jin@users.noreply.github.com>
2026-01-24 07:16:32 +0000

    Deploying to gh-pages from @ hyoo-ru/talks.hyoo.ru@898b5cfab226dee72e39478bcf11739d4934b7ed 🚀

--- node.d.ts
+++ node.d.ts
@@ -3127,6 +3127,7 @@ declare namespace $ {
 		bubble_content( ): readonly(any)[]
 		Filter( ): $mol_search
 		Trigger_icon( ): $mol_icon_dots_vertical
+		trigger_enabled( ): ReturnType< $mol_select['enabled'] >
 	}
 	
 }
