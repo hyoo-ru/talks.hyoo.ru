a700d4004f580c5a7863dc6a3aadeaf863636931
nin-jin <nin-jin@users.noreply.github.com>
2026-03-17 07:51:55 +0000

    Deploying to gh-pages from @ hyoo-ru/talks.hyoo.ru@898b5cfab226dee72e39478bcf11739d4934b7ed 🚀

--- node.mjs
+++ node.mjs
@@ -10209,6 +10209,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
