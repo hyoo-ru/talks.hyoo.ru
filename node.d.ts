6ca378b2554781600c2285bde047ee36cf83d5d4
nin-jin <nin-jin@users.noreply.github.com>
2026-07-09 10:20:10 +0000

    Deploying to gh-pages from @ hyoo-ru/talks.hyoo.ru@898b5cfab226dee72e39478bcf11739d4934b7ed 🚀

--- node.d.ts
+++ node.d.ts
@@ -4799,10 +4799,14 @@ declare namespace $ {
 		ReturnType< $mol_view['sub'] >
 	>
 	export class $mol_form_field extends $mol_labeler {
+		state( ): string | null
 		name( ): string
 		bid( ): string
 		Bid( ): $mol_view
 		control( ): any
+		attr( ): ({ 
+			'mol_form_field_state': ReturnType< $mol_form_field['state'] >,
+		})  & ReturnType< $mol_labeler['attr'] >
 		bids( ): readonly(string)[]
 		label( ): readonly(any)[]
 		content( ): readonly(any)[]
@@ -4816,6 +4820,7 @@ declare namespace $.$$ {
      * @see https://mol.hyoo.ru/#!section=demos/demo=mol_form_demo
      */
     class $mol_form_field extends $.$mol_form_field {
+        state(): string | null;
         bid(): string;
     }
 }
